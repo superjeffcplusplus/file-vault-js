2b1f9f54fb6324fe27f5aeb974db804403514ca1
Jean-François Pasche <jean-francois.pasche@heig-vd.ch>
2023-01-15 23:30:33 +0100

    add buttons

--- apps/client/src/components/Home.tsx
+++ apps/client/src/components/Home.tsx
@@ -1,4 +1,4 @@
-import {Stack, Typography} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
 import {Link} from "react-router-dom";
 
 export default function Home() {
@@ -11,12 +11,16 @@ export default function Home() {
         Powered by Javascript & NodeJs.
       </Typography>
       <Stack direction='row' justifyContent='space-evenly' alignItems='center' sx={{flex: 1}}>
-        <Link to={'/login'}>
-          <Typography>Login</Typography>
-        </Link>
-        <Link to={'/register'}>
-          <Typography>Register</Typography>
-        </Link>
+        <Button variant={'outlined'}>
+          <Link to={'/login'}>
+            <Typography>Login</Typography>
+          </Link>
+        </Button>
+        <Button variant={'outlined'}>
+          <Link to={'/register'}>
+            <Typography>Register</Typography>
+          </Link>
+        </Button>
       </Stack>
     </>
   );
